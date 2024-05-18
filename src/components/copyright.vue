9b17702070975417c1076f29725473c9c5f17053
Aehxy <im@aehxy.com>
2024-05-19 02:04:50 +0800

    萌备保留

--- src/components/copyright.vue
+++ src/components/copyright.vue
@@ -1,16 +1,15 @@
-<!--
- * @Author: ahmr-bot ahmrcxy@gmail.com
- * @Date: 2022-11-27 09:51:15
- * @LastEditors: ahmr-bot ahmrcxy@gmail.com
- * @LastEditTime: 2022-11-27 21:41:36
- * @FilePath: \MC\src\components\copyright.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
 <template>
     <v-footer>
         <v-col  class="text-center" >
         版权所有 © {{ new Date().getFullYear() }} — <strong href="https://aehxy.com">Aehxy</strong>
-        </v-col></v-footer>
+        </v-col>
+    </v-footer>
+    <v-footer>
+      添加萌ICP备案号
+    <v-col class="text-center" href="https://icp.gov.moe/?keyword=20226999">
+      萌ICP备20226999号
+    </v-col>
+    </v-footer>
         <v-footer v-for="list in lists">
           <v-col class="text-center" href="https://beian.miit.gov.cn">
            {{ list.icp  }}
@@ -18,8 +17,8 @@
         </v-footer>
 </template>
 <script setup>
-import { ref } from 'vue'
-import axios from 'axios'
+import axios from 'axios';
+import { ref } from 'vue';
 const lists = ref([])
 axios({
   method:'get',
